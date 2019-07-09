44992a93af7554e730b4626e33e12e7f6945682b
kai <b10223052@yuntech.org.tw>
2019-07-09 14:05:18 +0800

    init file

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+// import todoApp from './reducers'
+// import App from './components/App'
+
+// let store = createStore(todoApp)
+ReactDOM.render((
+  <div>
+    Hello world
+  </div>
+),document.getElementById('App'))
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('App')
+)
